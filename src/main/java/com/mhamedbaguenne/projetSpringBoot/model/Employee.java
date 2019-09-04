d638bc1b5456df53003437e54169381c26f8946e
Mhamed <mhamed.baguenne@soprasteria.com>
2019-09-04 16:45:21 +0200

    ajout de colonnes compléméntaire dans la table Employé

--- src/main/java/com/mhamedbaguenne/projetSpringBoot/model/Employee.java
+++ src/main/java/com/mhamedbaguenne/projetSpringBoot/model/Employee.java
@@ -9,16 +9,45 @@ public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
+    private String prenom;
     private String nom;
+    private String adresse;
+    private String numéro;
 
     public Employee() {
 
     }
 
-    public Employee(long id, String nom) {
+    public Employee(long id, String nom, String prenom, String adresse, String numero ) {
         this.id = id;
+        this.prenom = prenom;
         this.nom = nom;
+        this.adresse = adresse;
+        this.numéro = numero;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getNuméro() {
+        return numéro;
+    }
+
+    public void setNuméro(String numéro) {
+        this.numéro = numéro;
     }
 
     public long getId() {
